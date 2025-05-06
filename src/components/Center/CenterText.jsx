ad4967755a8175449b1fb63d9327fea864aee9bb
Malik Owais <owaisoff@gmail.com>
2025-05-06 16:16:36 +0500

    Select Updations

--- src/components/Center/CenterText.jsx
+++ src/components/Center/CenterText.jsx
@@ -4,7 +4,7 @@ import SubHeading from "./SubHeading";
 
 const CenterText = () => {
   return (
-    <div className="absolute text-center top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] ">
+    <div className="absolute text-center top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] select-none ">
       <HeroText />
       <SubHeading />
     </div>
